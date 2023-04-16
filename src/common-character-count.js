b20088288ee72fa24e1837d6ae4ad7de9a04682a
YaSidneva <yan-zublenk2008@narod.ru>
2023-04-16 15:11:21 +0200

    implement charter count task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let res = 0;
+  s2 = s2.split("");
+  console.log(s1);
+  for (let i = 0; i <= s1.length - 1; i++) {
+    let equalIndex = s2.indexOf(s1[i]);
+    if (equalIndex != -1) {
+      s2.splice(equalIndex, 1);
+      res += 1;
+    }
+  }
+  return res;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
